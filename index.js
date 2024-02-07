364a20132bb3d8d774aba842e19301ef64e645dd
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-07 22:07:25 +0500

    jbvkdvbda

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var n = 5;
+// var n = 5;
 
-for (var i = 1; i <= 10; i++) {
-  var jadval = n * i;
-  console.log(jadval);
-}
+// for (var i = 1; i <= 10; i++) {
+//   var jadval = n * i;
+//   console.log(jadval);
+// }
+
+// var n = 5;
+// var i = 1;
+
+// while (i <= 10) {
+//   var jadval = n * i;
+//   console.log(jadval);
+//   i++;
+// }
